4302ffd0dce0866d90f19fcf4977349fa466ba79
nguy2254 <quyen.nguyen@postgrad.otago.ac.nz>
2020-04-04 18:45:12 +1300

    Update romanToInt.py

--- romanToInt.py
+++ romanToInt.py
@@ -26,6 +26,16 @@ class Solution(object):
                         print(i, value_list, s)
                     elif s[i-1:i+1] in ['IV','IX','XL','XC','CD','CM']:
                         pass 
+def romanToInt(self, s: str) -> int:
+	res, prev = 0, 0
+	dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+	for i in s[::-1]:          # rev the s
+		if dict[i] >= prev:
+			res += dict[i]     # sum the value iff previous value same or more
+		else:
+			res -= dict[i]     # substract when value is like "IV" --> 5-1, "IX" --> 10 -1 etc 
+		prev = dict[i]
+	return res
                     else:
                         value_list.append(s[i])
                         print(i, value_list, s)
