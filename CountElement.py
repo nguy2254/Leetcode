5742e26480dd2469ab94f1551179d5b195ac659f
nguy2254 <quyen.nguyen@postgrad.otago.ac.nz>
2020-04-11 13:43:17 +1200

    Update CountElement.py

--- CountElement.py
+++ CountElement.py
@@ -11,3 +11,25 @@ class Solution(object):
         for i in dict:
             if dict.get(i+1) != None : count = count + dict[i] 
         return count 
+class Solution(object):
+    def countElements(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        point1= 0
+        point2= 1 
+        arr.sort()
+        count = 0
+        
+        for i in arr[:len(arr)-1]: 
+            if arr[point1] == arr[point2] - 1: 
+                count = count + point2 - point1
+                point1 = point2
+                point2 = point2 +1 
+            elif arr[point1] == arr[point2] : 
+                point2 = point2 +1 
+            else:
+                point1 = point2
+                point2 = point2 +1 
+        return count 
