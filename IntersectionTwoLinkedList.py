cf12204d8433f5ba8c25353899bc09678114411d
nguy2254 <quyen.nguyen@postgrad.otago.ac.nz>
2020-04-26 16:21:15 +1200

    Create IntersectionTwoLinkedList.py

---
+++ IntersectionTwoLinkedList.py
@@ -0,0 +1,7 @@
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        #Bruteforce
+    def getIntersectionNode(self, headA, headB):
